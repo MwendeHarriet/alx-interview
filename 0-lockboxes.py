636194417174768bd2d2b215f342bbe3b12f6f57
MwendeHarriet <mwendeharriet@gmail.com>
2024-03-11 02:16:52 -0700

    lockboxes

---
+++ 0-lockboxes.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""Module determines the number of boxes that can be opened."""
+
+def canUnlockAll(boxes):
+    """Function determines the number of boxes that can be opened."""
+    queue = []
+    unique = set()
+    unique.add(0)
+    for i in boxes[0]:
+        if i not in unique and i < len(boxes):
+            unique.add(i)
+            queue.append(i)
+
+    while len(queue):
+        for i in boxes[queue[0]]:
+            if i not in unique and i < len(boxes):
+                unique.add(i)
+                queue.append(i)
+        queue.pop(0)
+    if len(unique) == len(boxes):
+        return True
+    return False
